cefdf92c50343415efc9ff497de381258cf3707a
Mayuri-Saha18 <105915693+Mayuri-Saha18@users.noreply.github.com>
2023-04-26 23:09:43 +0530

    add

--- shoppingcart/src/components/Frontend/Products.jsx
+++ shoppingcart/src/components/Frontend/Products.jsx
@@ -1,21 +1,21 @@
 import React,{useState} from 'react'
-import data from "../back/data";
+import data from "../back/data"
 import { Box, Image,SimpleGrid,Text,Button } from '@chakra-ui/react';
 const Products = ({cartdata,handleCart,items}) => {
- // const {items} = data ;
-  // const [cartdata,setCartData]  = useState([]);
-  // const handleCart=(product)=>{
+//  const {items} = data ;
+//   const [cartdata,setCartData]  = useState([]);
+//   const handleCart=(product)=>{
        
-  //       const productExist = cartdata.find((el)=>el.id ===product.id) ;
-  //       if(productExist){
-  //           setCartData(cartdata.map((el)=> el.id === product.id ? {...productExist, quantity: productExist.quantity+1} : el))
+//         const productExist = cartdata.find((el)=>el.id ===product.id) ;
+//         if(productExist){
+//             setCartData(cartdata.map((el)=> el.id === product.id ? {...productExist, quantity: productExist.quantity+1} : el))
            
-  //         }
-  //       else{
-  //         setCartData([...cartdata , {...product,quantity :1}])
-  //       }
-  //       console.log(cartdata)
-  // }
+//           }
+//         else{
+//           setCartData([...cartdata , {...product,quantity :1}])
+//         }
+//         console.log(cartdata)
+//   }
   return (
     <Box className='products'><SimpleGrid  columns={{base:1,sm:2,md:3,lg:4}}  gap='20px' p='10px'>  
         {items.map((el)=>(
